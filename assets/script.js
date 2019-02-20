b065ff8dfd9e69f9d3365875debe6ea9e9c60a29
juliusdaub <julius@daubweb.com>
2019-02-20 23:48:18 +0100

    Experimente_Mit_Slideshow

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,35 @@ $('#sidebarCollapse').on('click', function () {
     $('#sidebar').toggleClass('active');
 });
 
+function carouselNormalization() {
+    var items = $('#carousel_slideshow .carousel-item'), //grab all slides
+        heights = [], //create empty array to store height values
+        tallest; //create variable to make note of the tallest slide
+
+    if (items.length) {
+        function normalizeHeights() {
+            items.each(function() { //add heights to array
+                heights.push($(this).height());
+            });
+            tallest = Math.max.apply(null, heights); //cache largest value
+            items.each(function() {
+                $(this).css('min-height',tallest + 'px');
+            });
+        };
+        normalizeHeights();
+
+        $(window).on('resize orientationchange', function () {
+            tallest = 0, heights.length = 0; //reset vars
+            items.each(function() {
+                $(this).css('min-height','0'); //reset min-height
+            });
+            normalizeHeights(); //run it again
+        });
+    }
+}
+
+
+
 jQuery(function ($) {
     $(".tile").height($("#reference_tile").innerWidth());
 
@@ -19,10 +48,10 @@ jQuery(function ($) {
         }, 10);
     });
     $(window).on('resizeEnd', function () {
+        carouselNormalization();
         if ($("#tile_container").outerWidth() > 335) {
             console.log("Resizing");
             $(".tile").height($("#reference_tile").innerWidth());
-
         } else {
             $(".tile").height(40);
             $(".tile").css("border", "0 0 0 0");
